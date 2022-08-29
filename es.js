f88c68c20cc36e3f2931c99b41ad6e2dd7b1e406
Chrissy F <chrissy.forster@yahoo.com>
2022-08-28 19:46:00 -0600

    made a clear button to clear screen of colors, need to create the js for the toggle button to change grid square size

--- es.js
+++ es.js
@@ -16,50 +16,15 @@ for (let pix = 0; pix < 256; pix++) {
     grid.style.backgroundColor = "#" + randomColors;
   }
 
-
-}
-const btn = document.getElementById("popUp");
-btn.addEventListener('click', () => getnewGrid());
-var size;
-function getnewGrid() {
-  size = prompt("How many squares per side would you like for the new grid? Hint max is 100x100.");
-  parseInt(size);
-  if (size > 100) {
-    size = prompt("Sorry! Max squares is 100 per side! Enter another lower number.");
-    size = parseInt(size);
-    removeGrid();
-    drawGrid();
-  }
-  else {
-    removeGrid();
-    drawGrid();
-  }
-
-}
-const container2 = document.createElement('div');
-container.appendChild(container2);
-function drawGrid() {
-
-  for (let pix = 0; pix < size * size; pix++) {
-    const grid1 = document.createElement("div");
-    grid1.setAttribute("id", "squares");
-    container2.appendChild(grid1);
-    grid1.setAttribute("class", "each1");
-
-    grid1.addEventListener("mouseover", colorChange);
-    function colorChange() {
-      var randomColors = Math.floor(Math.random() * 16777215).toString(16);
-      grid1.style.backgroundColor = "#" + randomColors;
+  const btn = document.getElementById("popUp");
+  btn.addEventListener('click', () => removeGrid());
+  //var size;
+  function removeGrid() {
+    for (var i = 0; i < 256; i++) {
+      grid.style.backgroundColor = "#0000ff";
     }
 
-
   }
 }
 
-function removeGrid() {
-  var divstoRemove = document.querySelectorAll(".each");
-  for (var i = 0; i < 256; i++) {
-    divstoRemove[i].remove();
-  }
 
-}
